66f7d8ff25e832929fdac8a0766d4e9a1d6e3f25
yuyutoolove <yuyutoolove@outlook.com>
2024-06-06 08:57:15 +0800

    Add files via upload

---
+++ AutoLoot/Init.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLoot
+{
+    public class ModAPI : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            ModEvents.GameStartDone.RegisterHandler(GameStartDone);
+        }
+
+        private void GameStartDone()
+        {
+            PatchHelper.DoPatches();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+七日杀 自动拾取 源码
+编译时，把 服务端目录/7DaysToDieServer_Data/Managed 中的dll文件拷贝进AutoLoot/7dtdlib中。
